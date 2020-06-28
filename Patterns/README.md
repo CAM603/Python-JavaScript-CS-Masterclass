92da9984a28c076a38d130a15170cd2b728b1688
CAM603 <cameronhawley@comcast.net>
2020-06-28 16:00:13 -0400

    add python frequency

--- Patterns/README.md
+++ Patterns/README.md
@@ -12,3 +12,11 @@
 
 -   This pattern uses objects or sets to collect values/frequencies of values
 -   This can often avoid the need for nested loops of O(n^2) operations with arrays/strings
+
+Example: Write a function called _same_, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+
+```js
+same([1, 2, 3], [4, 1, 9]); // true
+same([1, 2, 3], [1, 9]); // false
+same([1, 2, 1], [4, 4, 1]); // false (must be same frequency)
+```
---
+++ Patterns/frequency.py
@@ -0,0 +1,34 @@
+# def same(arr1, arr2):
+#     print(arr2)
+#     for num in arr1:
+#         if not num ** 2 in arr2:
+#             return False
+#         arr2.remove(num ** 2)
+
+#     return True
+
+
+def same(arr1, arr2):
+    counter1 = {}
+    counter2 = {}
+
+    for num in arr1:
+        if num not in counter1:
+            counter1[num] = 1
+        else:
+            counter1[num] += 1
+    for num in arr2:
+        if num not in counter2:
+            counter2[num] = 1
+        else:
+            counter2[num] += 1
+
+    for key in counter1:
+        if key ** 2 not in counter2:
+            return False
+        if counter1[key] is not counter2[key ** 2]:
+            return False
+    return True
+
+
+print(same([1, 2, 3, 2], [9, 1, 4, 4]))
