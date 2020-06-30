8283d0b6b8d5c11e810aeca966d641c003ee9bc0
CAM603 <cameronhawley@comcast.net>
2020-06-30 12:54:09 -0400

    add js multiple pointers problems

--- Patterns/multPointers.js
+++ Patterns/multPointers.js
@@ -96,5 +96,71 @@ function countUniqueValues(arr) {
 
 // CHALLENGE: averagePair
 /*
-
+Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+Bonus Constraints:
+Time: O(N)
+Space: O(1)
  */
+function averagePair(arr, target) {
+    let first = 0;
+    let second = arr.length - 1;
+
+    while (first < second) {
+        let ave = (arr[first] + arr[second]) / 2;
+        if (ave === target) {
+            return true;
+        }
+        if (ave < target) {
+            first++;
+        }
+        if (ave > target) {
+            second--;
+        }
+    }
+    return false;
+}
+
+averagePair([1, 2, 3], 2.5); // true
+averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
+averagePair([-1, 0, 2, 4, 5, 6], 4.1); // false
+averagePair([]); // false
+
+// CHALLENGE: isSubsequence
+/*
+Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without changing the ordering.
+Solution should have:
+Time: O(N + M)
+Space: O(1)
+*/
+function isSubsequence(str1, str2) {
+    let start = 0;
+    let runner = 0;
+
+    if (!str1) {
+        return true;
+    }
+
+    while (runner < str2.length) {
+        if (str1[start] === str2[runner]) {
+            start++;
+        }
+        // Start will be out of index if we continue the loop
+        if (start === str1.length) {
+            return true;
+        }
+        runner++;
+    }
+    return false;
+}
+
+function isSubsequence(str1, str2) {
+    if (str1.length === 0) return true;
+    if (str2.length === 0) return false;
+    if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+    return isSubsequence(str1, str2.slice(1));
+}
+
+isSubsequence("hello", "hello world"); // true
+isSubsequence("sing", "sting"); // true
+isSubsequence("abc", "abracadabra"); // true
+isSubsequence("abc", "acb"); // false (order matters)
