ab42aa2e28ae149029084e2a8baf3e0c58452180
CAM603 <cameronhawley@comcast.net>
2020-07-02 10:50:44 -0400

    finish js doubly linked list

--- DoublyLL/README.md
+++ DoublyLL/README.md
@@ -31,3 +31,91 @@ Removes the node from the end of the list
 -   set the new tail's next to be null
 -   decrement the length
 -   return the value removed
+
+### Shift
+
+Remove a node from the beginning of the list
+
+#### Psuedo Code
+
+-   if the length is 0, return undefined
+-   store the current head property in a variable
+-   if the length is 1, set the head and tail to be null
+-   update the head to be the next of the old head
+-   set the head's prev property to be null
+-   set the old head's next to be null
+-   decrement the length
+-   return the old head
+
+### Unshift
+
+Add a node to the beginning of the list
+
+#### Pseudo Code
+
+-   create a new node with the value passed into the function
+-   if the length is 0, set the head and tail to the new node
+-   else
+    -- set the prev property on the head of the list to be the new node
+    -- set the next property on the new node to be the head property
+    -- update the head to be the new node
+-   increment the length
+
+### Get
+
+Accessing a node by its position
+
+#### Psuedo Code
+
+-   if the index is less than 0 or greater than or equal the length, return null
+-   if the index is less than or equal to half the length:
+    -- loop through the list starting from the head
+    -- return node when it is found
+-   if the index is greater than half the length:
+    -- loop through the list starting from the tail
+    -- return the node when it is found
+
+### Set
+
+Replacing the value of a node in the list
+
+#### Pseudo Code
+
+-   create a variable which is the result of the get method at the index passed into the function
+    -- if the get method returns a valid node, set the value of that node to be the value passed to the function
+    -- return true
+-   otherwise return false
+
+### Insert
+
+Adding a node in a list by a certain position
+
+#### Psuedo Code
+
+-   if the index is < 0 or > length, return false
+-   if the index is 0, unshift
+-   if the index is the same as the length, push
+-   use get method to access the index - 1
+-   set the next and prev properties on the correct nodes to link everything together
+-   increment the length
+-   return true
+
+### Remove
+
+Remove a node at a certain position
+
+#### Pseudo Code
+
+-   if the index is < 0 or index >= length, return undefined
+-   if the index is 0, shift
+-   if the index is the same as the length - 1, pop
+-   use the get method to retrieve the to be removed
+-   update the next and prev properties to remove the found node from the list
+-   set next and prev to null on the found node
+
+## Big O
+
+Insert: O(1)
+Removal: O(1)
+Searching: O(N)
+Access: O(N)
--- DoublyLL/doublyLL.js
+++ DoublyLL/doublyLL.js
@@ -40,9 +40,101 @@ class DoublyLinkedList {
         this.length--;
         return popped;
     }
+    shift() {
+        if (!this.head) return undefined;
+        let shifted = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = shifted.next;
+            shifted.next = null;
+            this.head.prev = null;
+        }
+        this.length--;
+        return shifted;
+    }
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    get(index) {
+        let mid = Math.floor(this.length / 2);
+        if (index < 0 || index >= this.length) return null;
+        if (index <= mid) {
+            let count = 0;
+            let node = this.head;
+            while (count !== index) {
+                node = node.next;
+                count++;
+            }
+            return node;
+        } else {
+            let count = this.length - 1;
+            let node = this.tail;
+            while (count !== index) {
+                node = node.prev;
+                count--;
+            }
+            return node;
+        }
+    }
+    set(val, index) {
+        let node = this.get(index);
+        if (!node) return false;
+
+        node.val = val;
+        return true;
+    }
+    insert(val, index) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(val);
+        if (index === this.length) return !!this.push(val);
+
+        let newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let after = prev.next;
+
+        prev.next = newNode;
+        newNode.prev = prev;
+        newNode.next = after;
+        after.prev = newNode;
+
+        this.length++;
+        return true;
+    }
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        let removed = this.get(index);
+        let before = removed.prev;
+        let after = removed.next;
+
+        before.next = after;
+        after.prev = before;
+        removed.next = null;
+        removed.prev = null;
+
+        this.length--;
+        return removed;
+    }
 }
 let list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
 list.push(3);
-list.pop();
+list.push(4);
+list.push(5);
+list.push(6);
+list.remove(5);
