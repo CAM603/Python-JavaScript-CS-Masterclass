df6e6550b5e9651877bc36cb4c811928b7549b4d
CAM603 <cameronhawley@comcast.net>
2020-07-20 10:24:59 -0400

    add recursive isPalindrome

--- Recursion/JavaScript/challenging.js
+++ Recursion/JavaScript/challenging.js
@@ -1,6 +1,6 @@
 // Write a function called reverse which accepts a string and returns a new string reversed
-// reverse('awesome') -> 'emosewa'
-// reverse('rithmschool') -> 'loohcsmhtir'
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
 
 function reverse(str) {
     let reversed = "";
@@ -8,13 +8,36 @@ function reverse(str) {
     function helper(str) {
         if (str.length === 0) return;
 
-        let lastIndex = str.length - 1;
-
-        reversed += str[lastIndex];
-        helper(str.slice(0, lastIndex));
+        reversed += str[str.length - 1];
+        helper(str.slice(0, -1));
     }
     helper(str);
     return reversed;
 }
 
-reverse("awesome");
+function reversePure(str) {
+    let reversed = "";
+
+    if (str.length === 0) return reversed;
+
+    reversed = str[str.length - 1] + reversePure(str.slice(0, -1));
+    return reversed;
+}
+
+reversePure("awesome");
+
+// Write a recursive function called isPalindrome which returns true if the string passed in is palindrome (reads the same forward and backward) otherwise return false
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(str) {
+    if (str.length === 0) return true;
+
+    if (str[0] !== str[str.length - 1]) return false;
+
+    return isPalindrome(str.slice(1, -1));
+}
+isPalindrome("amanaplanacanalpanama");
